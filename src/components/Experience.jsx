545deb1c8fc906cd90a8b60ec33e00de896f4e80
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-07-24 17:47:43 +0300

    ADDED Multiple Animations to ./components/Avatar.jsx and Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,21 @@
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./index";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const {animation} = useControls({
+
+    animation: {
+      value:"Typing",
+      options: ["Typing", "Waving", "Falling" ]
+    }
+
+  });
   return (
     <>
       <OrbitControls />
       <group position-y={-1}>
-      <Avatar/>
+      <Avatar animation={animation}/>
       </group>
     <ambientLight intensity={1}/>
     </>
